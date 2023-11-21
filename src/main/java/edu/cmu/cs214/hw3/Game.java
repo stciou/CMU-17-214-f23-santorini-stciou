7c1a981ed0f9a9fc9e82a1112283a03c9e3d03cd
Shang-Ting Ciou <sciou@andrew.cmu.edu>
2023-11-21 00:48:56 -0500

    Set up Spring Boot Main class

--- src/main/java/edu/cmu/cs214/hw3/Game.java
+++ src/main/java/edu/cmu/cs214/hw3/Game.java
@@ -1,11 +1,8 @@
 package edu.cmu.cs214.hw3;
 
-import java.util.Scanner;
-
 /**
  * The {@code Game} class manages the flow and state of the game.
- * It initializes the game board, players, and controls the game loop,
- * ensuring that players take turns and the game state is updated accordingly.
+ * It initializes the game board, players, and controls the game loop.
  */
 public class Game {
     private Board board;
@@ -14,92 +11,37 @@ public class Game {
     private Player currentPlayer;
 
     /**
-     * Returns the game board.
-     * @return the game board.
-     */
-    public Player getPlayer1() {
-        return player1;
-    }
-
-    /**
-     * Constructs a new {@code Game} instance, initializing the board and players,
-     * and setting the starting player.
+     * Constructs a new {@code Game} instance, initializing the board and players.
      */
     public Game() {
         board = new Board();
         player1 = new Player(board);
         player2 = new Player(board);
         currentPlayer = player1; 
-
-        player1.placeWorkers(0, 0, 0, 1);
-        player2.placeWorkers(4, 4, 4, 3);
     }
 
     /**
-     * Starts the game loop, allowing players to take turns until the game ends.
-     * Announces the winner when the game is over.
-     */
-    public void startGame() {
-        while (true) {
-            boolean turnSuccessful = takeTurn();
-
-            if (!turnSuccessful) {
-                System.out.println("Game over! Player " + (currentPlayer == player1 ? "2" : "1") + " wins!");
-                break;
-            }
-
-            if (currentPlayer == player1) {
-                currentPlayer = player2;
-            } else {
-                currentPlayer = player1;
-            }
-        }
-    }
-
-    /**
-     * Manages a single turn for the current player, prompting them to make moves and build.
-     * Validates the player's input and updates the game state accordingly.
+     * Manages a single turn for the current player.
      *
+     * @param workerNum The worker number (1 or 2).
+     * @param moveToX   X-coordinate to move to.
+     * @param moveToY   Y-coordinate to move to.
+     * @param buildAtX  X-coordinate to build at.
+     * @param buildAtY  Y-coordinate to build at.
      * @return {@code true} if the turn was successful; {@code false} otherwise.
      */
-    public boolean takeTurn() {
-        Scanner scanner = new Scanner(System.in);
-    
-        System.out.println("Player " + (currentPlayer == player1 ? "1" : "2") + "'s turn.");
-    
-        int workerNum;
-        do {
-            System.out.print("Choose a worker (1 or 2): ");
-            workerNum = scanner.nextInt();
-        } while (workerNum != 1 && workerNum != 2);
-    
-        int moveToX, moveToY, buildAtX, buildAtY;
-        do {
-            System.out.print("Enter x-coordinate to move to: ");
-            moveToX = scanner.nextInt();
-    
-            System.out.print("Enter y-coordinate to move to: ");
-            moveToY = scanner.nextInt();
-        } while (!board.isValidPosition(moveToX, moveToY) || board.getCell(moveToX, moveToY).isOccupied());
-    
-        do {
-            System.out.print("Enter x-coordinate to build at: ");
-            buildAtX = scanner.nextInt();
-    
-            System.out.print("Enter y-coordinate to build at: ");
-            buildAtY = scanner.nextInt();
-        } while (!board.isValidPosition(buildAtX, buildAtY) || board.getCell(buildAtX, buildAtY).isOccupied());
-    
-        return currentPlayer.takeTurn(workerNum, moveToX, moveToY, buildAtX, buildAtY);
+    public boolean takeTurn(int workerNum, int moveToX, int moveToY, int buildAtX, int buildAtY) {
+        boolean turnSuccessful = currentPlayer.takeTurn(workerNum, moveToX, moveToY, buildAtX, buildAtY);
+        if (turnSuccessful) {
+            switchCurrentPlayer();
+        }
+        return turnSuccessful;
     }
 
     /**
-     * The entry point of the application. Creates a new game instance and starts it.
-     *
-     * @param args command-line arguments (not used).
+     * Switches the current player.
      */
-    public static void main(String[] args) {
-        Game game = new Game();
-        game.startGame();
+    private void switchCurrentPlayer() {
+        currentPlayer = (currentPlayer == player1) ? player2 : player1;
     }
 }
